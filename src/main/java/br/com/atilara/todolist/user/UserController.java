410e17be6959eaeec4d00d1eb6da6c6c521acc9b
Átila Rodrigues <atila.rodriguesal@gmail.com>
2023-10-12 16:31:28 -0300

    feat: encrypt user password

--- src/main/java/br/com/atilara/todolist/user/UserController.java
+++ src/main/java/br/com/atilara/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.atilara.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,10 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
         }
+
+        var hashedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(hashedPassword);
 
         return ResponseEntity.ok(this.userRepository.save(userModel));
     }
