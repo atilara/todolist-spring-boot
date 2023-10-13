12009331df0b0d0fa16e8c09a422a7bcb08371cf
Átila Rodrigues <atila.rodriguesal@gmail.com>
2023-10-13 17:27:21 -0300

    feat: add routes to list and update tasks

--- src/main/java/br/com/atilara/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/atilara/todolist/task/ITaskRepository.java
@@ -2,7 +2,11 @@ package br.com.atilara.todolist.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+    List<TaskModel> findByUserId(UUID userId);
+
 }
--- src/main/java/br/com/atilara/todolist/task/TaskController.java
+++ src/main/java/br/com/atilara/todolist/task/TaskController.java
@@ -1,13 +1,12 @@
 package br.com.atilara.todolist.task;
 
+import br.com.atilara.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -21,7 +20,36 @@ public class TaskController {
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var userId = (UUID) request.getAttribute("userId");
         taskModel.setUserId(userId);
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.badRequest().body("A data de início não pode ser maior que a data de término");
+        }
+
         return ResponseEntity.ok(this.taskRepository.save(taskModel));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity list (HttpServletRequest request) {
+        var userId = (UUID) request.getAttribute("userId");
+        return ResponseEntity.ok(this.taskRepository.findByUserId(userId));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        var userId = (UUID) request.getAttribute("userId");
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (!task.getUserId().equals(userId)) {
+            return ResponseEntity.status(403).build();
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return ResponseEntity.ok(this.taskRepository.save(task));
     }
 
 }
